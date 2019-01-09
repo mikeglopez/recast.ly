f37fd2bcfa272d3b982761210c14c6ca1656db72
Mike Lopez <mikeglopez.91@gmail.com>
2019-01-09 00:08:45 -0600

    Begin Implementing VideoList with exampleVideoData

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,14 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
+    <div><h5><em>{props.exampleVideoData.map(video =>
+      <VideoListEntry video={video} />
+    )}
+    </em> view goes here</h5>
+    </div>
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
